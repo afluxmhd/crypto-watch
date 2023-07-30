a72c1cd912c60f01d71f292c7440d46c99b79d52
Aflah <93920274+afluxmhd@users.noreply.github.com>
2023-07-30 12:50:38 +0530

    Update app_constants.dart

--- lib/utils/app_constants.dart
+++ lib/utils/app_constants.dart
@@ -1,6 +1,6 @@
 class AppConstants {
   static const String BASE_URL = "https://rest.coinapi.io";
-  static const String API_KEY = 'EC1F0775-9F7C-447F-917B-F311C6450909';
+  static const String API_KEY = 'YOUR_API_KEY';
 
   //End Points
   static const String EXCHANGE_RATE_PATH = "/v1/exchangerate/";
